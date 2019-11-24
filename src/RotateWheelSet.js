5570b0621c6597dd7ee1a08fff1ba54d7e4cce46
Jeremy Larter <jeremylarter@Jeremys-MBP.gateway>
2019-11-24 21:18:05 +1100

    Add spinning state and spin counter

--- src/RotateWheelSet.js
+++ src/RotateWheelSet.js
@@ -3,20 +3,22 @@ import DisplayWheelSet from './DisplayWheelSet';
 
 const RotateWheelSet = (props) => {
     const [timer, setTimer] = useState(0);
+    const [spinCounter, setSpinCounter] = useState(0);
+    const [spinning, setSpinning] = useState(false);
+    const spinningRef = useRef();
     const requestRef = useRef();
     const functionRef = useRef();
     const deltaRef = useRef({last:0, change:0});
-    const [loopMax, setLoopMax] = useState(0);
     const minDelay = 100;
-    const minItemMoves = 1000;//todo: this does not make sense anymore. If the wheels have not settled, it could lead to wrong display.
+    const loopMax = 1000;
     const wheelIndexMax = 11; //todo: remove this and use DisplayWheelSet values.
-    const log = (message, fn) => {//decorator
-        return () => {
-            const result = fn();
-            console.log(message, result);
-            return result;
-        }
-    }
+    // const log = (message, fn) => {//decorator
+    //     return () => {
+    //         const result = fn();
+    //         console.log(message, result);
+    //         return result;
+    //     }
+    // }
     const getRandomWheelIndex = () => Math.floor(Math.random() * wheelIndexMax);
     const getNextPosition = () => ({
         left: getRandomWheelIndex(),
@@ -31,6 +33,7 @@ const RotateWheelSet = (props) => {
     const wheelsDeltaRef = useRef(initWheelsDeltaRef);
     const minWheelSpinTime = 2000;//todo: change this to different intervals - 4000, 1000, 1000
     const loop = useCallback( time => {
+        functionRef.current += 1;//todo: rename this as loopGuard. When developing, we do not want an infinite loop.
         const wheelIncrement = (currentIndex, nextIndex, wheelTimePassed) => currentIndex === nextIndex && wheelTimePassed ? 0 : 1;
         if (wheelsDeltaRef.current.last === 0) {
             //first iteration, the change should be zero
@@ -55,11 +58,14 @@ const RotateWheelSet = (props) => {
         }
         setTimer(() => deltaRef.current.change);
         if (deltaRef.current.change > minDelay) {
-            functionRef.current += 1;//todo: remove this? maybe use as a flag for minimum number of iterations?
             deltaRef.current.last = time;
             const rightIncrement = wheelIncrement(positionRef.current.position.right % 11, positionRef.current.next.right, wheelsDeltaRef.current.timePassed.right);
             if (rightIncrement === 0) {
-                functionRef.current = loopMax;//todo: this is not readable. We are setting a different loop exit condition.
+                spinningRef.current = false;
+                setSpinning(() => spinningRef.current);
+                if (functionRef.current > loopMax) {
+                    spinningRef.current = false;
+                }
             }
             positionRef.current.position = {
                 left: positionRef.current.position.left + wheelIncrement(positionRef.current.position.left % 11, positionRef.current.next.left, wheelsDeltaRef.current.timePassed.left),
@@ -70,28 +76,34 @@ const RotateWheelSet = (props) => {
         }
         //todo: can we smooth out the scroll using css animation?
         //todo: cannot stop just because right wheel timer is up, need to wait to position is settled on.
-        if (functionRef.current < loopMax) {
+        if (spinning && spinningRef.current && functionRef.current < loopMax) {
+            //todo: what is a better pattern than using an always true value (spinning - due to closure?) to stop lint warning?
             requestRef.current = window.requestAnimationFrame(loop);
         }
-    }, [loopMax, positionRef]);
+    }, [spinning]);
 
     const cancelSpin = () => window.cancelAnimationFrame(requestRef.current);
-
     const spin = () => {
         //todo: only allow another spin after the current spin has finished.
         //idea: use css scroll stop position and allow the user to see all the items on a wheel
-        setLoopMax(previous => previous + minItemMoves);
+        spinningRef.current = true;
+        setSpinning(() => spinningRef.current);
+        setSpinCounter(previous => previous + 1);//todo: what happens when overflow occurs? max float + 1?
+        //setLoopMax(previous => previous + minItemMoves);
         wheelsDeltaRef.current = initWheelsDeltaRef;
-        const comparisonPosition = {left: positionRef.current.position.left % 11, center: positionRef.current.position.center % 11, right: positionRef.current.position.right % 11};
-        console.log("current: ", comparisonPosition, positionRef.current.position);
-        positionRef.current.next = log("spin: ", getNextPosition)();
+        //const comparisonPosition = {left: positionRef.current.position.left % 11, center: positionRef.current.position.center % 11, right: positionRef.current.position.right % 11};
+        //console.log("current: ", comparisonPosition, positionRef.current.position);
+        //positionRef.current.next = log("spin: ", getNextPosition)();
+        positionRef.current.next = getNextPosition();
     }
 
     useEffect(() => {
         if (functionRef.current === undefined) {
+            spinningRef.current = false;
             functionRef.current = props.startPosition.left;//1
         } else { //delay the loop until after first page load.
             requestRef.current = window.requestAnimationFrame(loop);
+            functionRef.current = 0;//todo: rename this as loopGuard. When developing, we do not want an infinite loop.
         }
 
         return () => {
@@ -102,9 +114,9 @@ const RotateWheelSet = (props) => {
 
     return (
         <div>
-            <button onClick={cancelSpin}>cancel</button><br />
-            <button onClick={spin}>spin</button><br />
-            {timer}
+            <button onClick={cancelSpin} disabled={!spinning}>break</button><br />
+            <button onClick={spin} disabled={spinning}>spin</button><br />
+            count: {spinCounter} {spinning ? "spinning" : "stopped"} {timer}
             <DisplayWheelSet startPosition={currentPosition} />
         </div>
     );
