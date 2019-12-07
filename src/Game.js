48e663d7b317a3fff7a49baf0cc4ebac4a76c4a3
Jeremy Larter <jeremylarter@Jeremys-MBP.gateway>
2019-12-07 14:55:58 +1100

    Refactor credit out of rotate wheel set Simplify game to rotate coupling Make wheel settling more consistent by flaging wheel not settled Removed win output

--- src/Game.js
+++ src/Game.js
@@ -1,48 +1,71 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useRef} from 'react';
 import RotateWheelSet from './RotateWheelSet'
 import DisplayCredit from './DisplayCredit';
 import DisplayWin from './DisplayWin';
 
 const Game = () => {
-    const [credit, setCredit] = useState(5);
+    const render = useRef(0);
+    const [credit, setCredit] = useState(50);
     const insertCreditAmount = 1;
     const insertCredit = creditAmount => () => setCredit(_ => _ + creditAmount);
 
     const wheelIndexMax = 11; //todo: remove this and use DisplayWheelSet values.
     const [win, setWin] = useState();
-    const betFinished = () => { setWin(_ => true); };
+    const [betSwitch, setBetSwitch] = useState(false);//todo: initialise only once
     const getRandomWheelIndex = () => Math.floor(Math.random() * wheelIndexMax);
     const getNextPosition = () => {
-        setWin(false);
+        console.log('getNextPosition');
         return {
             left: getRandomWheelIndex(),
             center: getRandomWheelIndex(),
             right: getRandomWheelIndex()
         }
     };
-    useEffect(() => {
-        if (win === undefined) {
-            setWin(false);
+    // useEffect(() => {
+    //     if (win === undefined) {
+    //         setWin(false);
+    //     }
+    //     console.log("win changed: " + win);
+    //     //return () => {};
+    // }, [win]);
+    const spendAmount = 1;
+    const reduceCredit = spendAmount => () => setCredit(_ => _ - spendAmount);
+    const [customerAlert, setCustomerAlert] = useState("");
+    const targetPosition = useRef();
+    const spendCredit = spendAmount => () => {
+        console.log('spend amount');
+        if (credit > 0) {
+            setCustomerAlert("");
+            reduceCredit(spendAmount)();
+            targetPosition.current = getNextPosition();
+            setBetSwitch(_ => !_);//toggle the switch e.g. off - on, on - off
+            setBetInProgress(() => true);
+        } else {
+            setCustomerAlert("Please insert credit to continue.");
         }
-        //console.log("win changed: " + win);
-        return () => {
-            ;
-        };
-    }, [win]);
+    }
+    const [betInProgress, setBetInProgress] = useState(false);
+    const betFinished = () => {
+        console.log('target position found');
+        setBetInProgress(() => false);
+        setWin(() => true);
+    };
 
     return (
         <DisplayCredit credit={credit} >
+            Game: {render.current++}
             <button onClick={insertCredit(insertCreditAmount)}>insert {insertCreditAmount} credit{insertCreditAmount > 1 ? "s" : ""}</button><br />
+            <button onClick={spendCredit(spendAmount)} disabled={betInProgress}>bet</button><br />{customerAlert}
 
             <RotateWheelSet 
+                betSwitch={betSwitch} callback={betFinished}
                 startPosition={{
                     left: 0,
                     center: 0,
                     right: 0
                 }}
                 credit={credit} setCredit={setCredit}
-                getNextPosition={getNextPosition}
-                betFinished={betFinished}
+                targetPosition={targetPosition.current}
             />
             <DisplayWin win={win} />
         </DisplayCredit>
--- src/RotateWheelSet.js
+++ src/RotateWheelSet.js
@@ -1,16 +1,15 @@
-import React, {useState, useRef, useEffect, useCallback} from 'react';
+import React, {useState, useRef, useEffect, memo} from 'react';
 import DisplayWheelSet from './DisplayWheelSet';
 import FramesPerSecond from './FramesPerSecond';
 
-const RotateWheelSet = (props) => {
-    const minDelay = 50;//number of milliseconds that it takes an item to move up one spot.
-    const minWheelSpinTime = 100;//todo: change this to different intervals - 4000, 1000, 1000
+const RotateWheelSet = ({targetPosition, betSwitch, callback, ...props}) => {
+    const minDelay = 100;//number of milliseconds that it takes an item to move up one spot.
+    const minWheelSpinTime = 1000;//todo: change this to different intervals - 4000, 1000, 1000
     const [timer, setTimer] = useState(minDelay);//todo: what is the value proposition for useState over useRef?
     const [spinCounter, setSpinCounter] = useState(0);
     const spinningRef = useRef();
     const requestRef = useRef();
-    const functionRef = useRef();
-    const dummyRef = useRef();
+    const loopGuardRef = useRef(0);
     const deltaRef = useRef({last:0, change:0});
     const loopMax = 10000;//infinite loop guard: maximum number of elapsed frames without settling on next position before we quit
 
@@ -21,114 +20,141 @@ const RotateWheelSet = (props) => {
     const initWheelsDeltaRef = {last:0, change:0, timePassed: initTimePassed};
     const wheelsDeltaRef = useRef(initWheelsDeltaRef);
     const deltaTime = useRef(0);
-    const loop = useCallback( time => {
-        deltaTime.current = time;
-        functionRef.current += 1;//todo: rename this as loopGuard. When developing, we do not want an infinite loop.
-        const wheelIncrement = (currentIndex, nextIndex, wheelTimePassed) => currentIndex === nextIndex && wheelTimePassed ? 0 : 1;
-        if (wheelsDeltaRef.current.last === 0) {
-            //first iteration, the change should be zero
-            wheelsDeltaRef.current.last = time;
-        }
-        deltaRef.current.change = time - deltaRef.current.last;
-        wheelsDeltaRef.current.change = time - wheelsDeltaRef.current.last;
-        if (wheelsDeltaRef.current.change > minWheelSpinTime) {
-            //a wheel can stop now
-            wheelsDeltaRef.current.last = time;
-            if (!wheelsDeltaRef.current.timePassed.left) {
-                wheelsDeltaRef.current.timePassed.left = true;
-            } else {
-                //center or right wheel
-                if (!wheelsDeltaRef.current.timePassed.center) {
-                    wheelsDeltaRef.current.timePassed.center = true;
+    const flipped = useRef();
+    const waitForWheelStop = useRef({left: false, center: false, right: false});
+    const cancelSpin = () => window.cancelAnimationFrame(requestRef.current);
+//https://reactjs.org/docs/composition-vs-inheritance.html
+//https://w3bits.com/svg-sprites/
+//https://bl.ocks.org/helderdarocha/8b28505082bf1c81977d7dec797686c7
+//https://reactjs.org/docs/hooks-effect.html
+//https://daveceddia.com/useeffect-hook-examples/
+//https://www.javascriptstuff.com/component-communication/#3-callback-functions
+//https://reactjs.org/docs/context.html
+    useEffect(() => {
+        const loop = time => {
+            console.log('loop');
+            //console.log(time);
+            //todo: the time delay and position settled logic is wrong.
+            deltaTime.current = time;
+            if (wheelsDeltaRef.current.last === 0) {
+                //first iteration, the change should be zero
+                wheelsDeltaRef.current.last = time;
+            }
+            deltaRef.current.change = time - deltaRef.current.last;
+            wheelsDeltaRef.current.change = time - wheelsDeltaRef.current.last;
+            //console.log("wheelsDeltaRef.current.change: ", wheelsDeltaRef.current.change);
+            if (wheelsDeltaRef.current.change > minWheelSpinTime && 
+                !waitForWheelStop.current.left && 
+                !waitForWheelStop.current.center &&
+                !waitForWheelStop.current.right) {
+                //console.log('stopping wheel from now', wheelsDeltaRef.current.change);
+                if (!wheelsDeltaRef.current.timePassed.left) {
+                    wheelsDeltaRef.current.timePassed.left = true;
+                    waitForWheelStop.current.left = true;
+                    console.log('stopping left wheel from now', wheelsDeltaRef.current.change);
                 } else {
-                    //right
-                    wheelsDeltaRef.current.timePassed.right = true;
+                    //center or right wheel
+                    if (!wheelsDeltaRef.current.timePassed.center) {
+                        wheelsDeltaRef.current.timePassed.center = true;
+                        waitForWheelStop.current.center = true;
+                        console.log('stopping center wheel from now', wheelsDeltaRef.current.change);
+                    } else {
+                        //right
+                        wheelsDeltaRef.current.timePassed.right = true;
+                        waitForWheelStop.current.right = true;
+                        console.log('stopping right wheel from now', wheelsDeltaRef.current.change);
+                    }
                 }
             }
-        }
-        setTimer(() => Math.floor(deltaRef.current.change));
-        const hasNext = positionRef.current.next;
-        if (deltaRef.current.change > minDelay && hasNext) {
-            deltaRef.current.last = time;
-            const rightIncrement = wheelIncrement(positionRef.current.position.right % 11, positionRef.current.next.right, wheelsDeltaRef.current.timePassed.right);
-            if (rightIncrement === 0) {
-                //cannot stop just because right wheel timer is up, need to wait to right position is settled on.
-                spinningRef.current = false;
-                props.betFinished();
+            setTimer(() => Math.floor(deltaRef.current.change));
+            const hasNext = positionRef.current.next;
+            const wheelIncrement = (currentIndex, nextIndex, wheelTimePassed) => currentIndex === nextIndex && wheelTimePassed ? 0 : 1;
+            if (deltaRef.current.change > minDelay && hasNext) {
+                deltaRef.current.last = time;
+                const leftIncrement = wheelIncrement(positionRef.current.position.left % 11, positionRef.current.next.left, wheelsDeltaRef.current.timePassed.left);
+                const centerIncrement = wheelIncrement(positionRef.current.position.center % 11, positionRef.current.next.center, wheelsDeltaRef.current.timePassed.center);
+                const rightIncrement = wheelIncrement(positionRef.current.position.right % 11, positionRef.current.next.right, wheelsDeltaRef.current.timePassed.right);
+                //check for stop and reset min wait for the next wheel
+                if (waitForWheelStop.current.left && leftIncrement === 0 && centerIncrement === 1) {
+                    wheelsDeltaRef.current.last = time;
+                    console.log('left settled', wheelsDeltaRef.current.change);
+                    waitForWheelStop.current.left = false;
+                }
+                if (waitForWheelStop.current.center && centerIncrement === 0 && rightIncrement === 1) {
+                    wheelsDeltaRef.current.last = time;
+                    console.log('center settled', wheelsDeltaRef.current.change);
+                    waitForWheelStop.current.center = false;
+                }
+
+                if (rightIncrement === 0) {
+                    //wait to right position is settled on before stopping
+                    spinningRef.current = false;
+                    console.log('right settled', wheelsDeltaRef.current.change);
+                    waitForWheelStop.current.right = false;//todo: I think this is not needed...
+                }
+                positionRef.current.position = {
+                    left: positionRef.current.position.left + leftIncrement,
+                    center: positionRef.current.position.center + centerIncrement,
+                    right: positionRef.current.position.right + rightIncrement
+                };
+                setCurrentPosition(() => positionRef.current.position);
             }
-            positionRef.current.position = {
-                left: positionRef.current.position.left + wheelIncrement(positionRef.current.position.left % 11, positionRef.current.next.left, wheelsDeltaRef.current.timePassed.left),
-                center: positionRef.current.position.center + wheelIncrement(positionRef.current.position.center % 11, positionRef.current.next.center, wheelsDeltaRef.current.timePassed.center),
-                right: positionRef.current.position.right + rightIncrement
-            };
-            setCurrentPosition(() => positionRef.current.position);
+            // //todo: can we smooth out the scroll using css animation?
+            if (spinningRef.current && loopGuardRef.current < loopMax) {
+                loopGuardRef.current += 1;
+                //console.log('recursive loop call: ', requestRef.current);
+                requestRef.current = window.requestAnimationFrame(loop);
+                //console.log('recursive loop call after', requestRef.current);
+            } else {
+                callback();//tell caller we are done, can this be async?
+            }
+        };
+        if (flipped.current === undefined && betSwitch !== undefined) {
+            //console.log('flipped init state not triggering');
+            flipped.current = !betSwitch;//to trigger on page load, reverse the boolean
         }
-        //todo: can we smooth out the scroll using css animation?
-        if (spinningRef.current && functionRef.current < loopMax) {
+        //console.log('flipping test: ', flipped.current, betSwitch);
+        if (flipped.current === betSwitch) {
+            flipped.current = !betSwitch;//do not use: flipped.current = !flipped.current;//dangerous, because other values can cause even number of renders
+
+            //idea: use css scroll stop position and allow the user to see all the items on a wheel
+            const resetTimePassed = {left: false, center: false, right: false};
+            const resetWheelsDeltaRef = {last:0, change:0, timePassed: resetTimePassed};
+            wheelsDeltaRef.current = resetWheelsDeltaRef;
+            const comparisonPosition = {left: positionRef.current.position.left % 11, center: positionRef.current.position.center % 11, right: positionRef.current.position.right % 11};
+            console.log("current: ", comparisonPosition, positionRef.current.position, targetPosition);
+            positionRef.current.next = targetPosition;
+            setSpinCounter(previous => previous + 1);//todo: what happens when overflow occurs? max float + 1?
+            spinningRef.current = true;//note, the reason this works is because spinCounter triggers a render, and at that time the ref is evaluated.
+            //if the only thing changing is the ref, then the render will not trigger, and the toggle logic will fail.
             requestRef.current = window.requestAnimationFrame(loop);
         }
-        dummyRef.current = spinCounter;//used to stop React complaining
-    }, [spinCounter, props]);//end loop callback
-
-    const reduceCredit = spendAmount => () => props.setCredit(_ => _ - spendAmount);
-    const cancelSpin = () => window.cancelAnimationFrame(requestRef.current);
-    const [customerAlert, setCustomerAlert] = useState("");
-    const spendCredit = spendAmount => () => {
-        if (props.credit > 0) {
-            reduceCredit(spendAmount)();
-            spin();
-        } else {
-            setCustomerAlert("Please insert credit to continue.");
-        }
-    }
-
-    const spin = () => {
-        //idea: use css scroll stop position and allow the user to see all the items on a wheel
-        spinningRef.current = true;
-        setSpinCounter(previous => previous + 1);//todo: what happens when overflow occurs? max float + 1?
-        wheelsDeltaRef.current = initWheelsDeltaRef;
-        //const comparisonPosition = {left: positionRef.current.position.left % 11, center: positionRef.current.position.center % 11, right: positionRef.current.position.right % 11};
-        //console.log("current: ", comparisonPosition, positionRef.current.position);
-        //positionRef.current.next = log("spin: ", getNextPosition)();
-        positionRef.current.next = props.getNextPosition();
-    }
-
-    useEffect(() => {
-        if (props.credit > 0) {
-            setCustomerAlert("");
-        }
-        if (functionRef.current === undefined) {
-            spinningRef.current = false;
-        } else { //delay the loop until after first page load.
-            if (spinningRef.current) {//todo: check what causes useEffect to trigger when spinningRef.current === false
-                requestRef.current = window.requestAnimationFrame(loop);
-            }
-        }
-        functionRef.current = 0;//todo: rename this as loopGuard. When developing, we do not want an infinite loop.
 
         return () => {
-            //stop the animation when disposed
-            window.cancelAnimationFrame(requestRef.current)
+            console.log('dispose rotate wheel set?', requestRef.current);
+            //todo: why are there 2 dispose calls for the initial spin?
+            loopGuardRef.current = 0;
+            waitForWheelStop.current = {left: false, center: false, right: false};
+            //stop the animation when disposed?
+            //if (cancelAnimationFrameId) window.cancelAnimationFrame(cancelAnimationFrameId);
         };
-    }, [loop, props.credit]);
-    const debug = false;
+    }, [betSwitch, callback, targetPosition]);//todo: maybe instead of betSwitch, targetPosition could be used? 
+    //Nope, because it is possible to get the same spin twice in a row and we need the spin to trigger
+    const debug = true;
     const debugOutput = (
         <>
             <button onClick={cancelSpin} disabled={!spinningRef.current}>break</button><br />
             {`count: ${spinCounter} ${spinningRef.current ? "spinning" : "stopped"} ${timer} `}<br />
             <FramesPerSecond time={deltaTime.current} animationId={spinCounter} /><br />
-            Credit: {props.credit}<br />
         </>
     );
-    const spendAmount = 1;
 
     return (
         <div>
-            <button onClick={spendCredit(spendAmount)} disabled={spinningRef.current}>bet</button><br />{customerAlert}
             {debug ? debugOutput : ""}
             <DisplayWheelSet startPosition={currentPosition} debug={debug} />
         </div>
     );
 }
 
-export default RotateWheelSet;
+export default memo(RotateWheelSet);
